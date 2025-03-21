648ee8b2136b4edbce39fd536bfab509a3216d02
dwlee <dwlee@eutopos.co.kr>
2025-03-22 07:57:34 +0900

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-app/' // repository 이름을 base URL로 설정
 })
